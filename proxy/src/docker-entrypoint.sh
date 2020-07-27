499bd0a6fa4b92a6f0960722876b1321d75e6063
Akira Kusumoto <gkusumoto@gmail.com>
2020-07-27 21:21:58 +0900

    update nginx settings

--- proxy/src/docker-entrypoint.sh
+++ proxy/src/docker-entrypoint.sh
@@ -4,15 +4,28 @@ set -x
 
 export LANG=C.UTF-8
 
-if [ ! -e /etc/letsencrypt/live/www.akusumoto.com/fullchain.pem -o ! -e /etc/letsencrypt/live/www.akusumoto.com/privkey.pem ]
+DEFAULT_CERT=/default/letsencrypt/live/www.akusumoto.com/fullchain.pem
+DEFAULT_CERTKEY=/default/letsencrypt/live/www.akusumoto.com/privkey.pem
+CERT=/etc/letsencrypt/live/www.akusumoto.com/fullchain.pem
+CERTKEY=/etc/letsencrypt/live/www.akusumoto.com/privkey.pem
+
+if [ ! -e ${CERT} -o ! -e ${CERTKEY} ]
 then
-    if [ -e /default/letsencrypt/live/www.akusumoto.com/fullchain.pem -a -e /default/letsencrypt/live/www.akusumoto.com/privkey.pem ]
+    if [ -e ${DEFAULT_CERT} -a -e ${DEFAULT_CERTKEY} ]
     then
-        # copy certificate from /default/letsencrypt
-        cp -rf /default/letsencrypt/* /etc/letsencrypt/
+	openssl x509 -noout -checkend 86400 -in ${DEFAULT_CERT} 
+	if [ $? -eq 0 ]; then
+            # the certificate will no expire within 1 day
+	    # copy certificate from /default/letsencrypt
+            cp -rf /default/letsencrypt/* /etc/letsencrypt/
+	else
+            # the certificate will expire within 1 day or is already expired
+            # initialize certificate and setting
+            certbot certonly --standalone -d www.akusumoto.com -m gkusumoto@gmail.com --agree-tos -n
+	fi
     else
         # initialize certificate and setting
-        certbot certonly -d www.akusumoto.com -m gkusumoto@gmail.com --agree-tos -n
+        certbot certonly --standalone -d www.akusumoto.com -m gkusumoto@gmail.com --agree-tos -n
         #certbot --standalone -d www.akusumoto.com -m gkusumoto@gmail.com --nginx --agree-tos -n
     fi
 fi
